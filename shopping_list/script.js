70417aa4108c099d76a5ae2bde76862521cfcfa4
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2024-08-27 18:08:08 +0200

    The back is finished

--- shopping_list/script.js
+++ shopping_list/script.js
@@ -4,6 +4,7 @@
 const input = document.getElementById("input");
 const saveButton = document.getElementById("saveButton");
 const shoppingList = document.getElementById("shoppingList");
+const clearAll = document.getElementById("clearAll");
 // create an object array
 let itemObject = [];
 
@@ -13,72 +14,89 @@ const addItemToList = () => {
 
     if (cleanInput !== "") {
         // verfication
-        let itemExist = itemObject.find(item => item.name === cleanInput);
-
-        if (itemExist){
+        let itemExist = itemObject.find((item) => item.name === cleanInput);
+        // add if the input exist
+        if (itemExist) {
             itemExist.quantity++;
-        }else{
-            const newItem = {name: cleanInput, quantity: 1};
+            const getCount = document.getElementById(`count_${cleanInput}`);
+            getCount.textContent = itemExist.quantity;
+            // create item
+        } else {
+            const newItem = { name: cleanInput, quantity: 1 };
             itemObject.push(newItem);
-        // create item
-        const itemValue = document.createElement("li");
-        itemValue.textContent = cleanInput;
-        // create delete button
-        const deleteButton = document.createElement("button");
-        deleteButton.textContent = "Delete";
-        deleteButton.classList.add("deleteButton");
-        deleteButton.style.marginLeft = "10px";
-        // add event
-        deleteButton.addEventListener("click", () => {
-            shoppingList.removeChild(itemValue);
-            itemObject = itemObject.filter(item => item.name !== cleanInput);
-        })
-        // create - button
-        const removeButton = document.createElement("button");
-        removeButton.textContent = "-";
-        removeButton.classList.add("removeButton");
-        removeButton.style.marginLeft = "10px";
-        // add event
-        removeButton.addEventListener("click", () =>{
-            if (newItem.quantity > 1) {
-                newItem.quantity--;
-                count.textContent = newItem.quantity;
-            }else {
+            // create item
+            const itemValue = document.createElement("li");
+            itemValue.textContent = cleanInput;
+            // create delete button
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = "Delete";
+            deleteButton.id = "deleteButton";
+            deleteButton.classList.add("deleteButton");
+            deleteButton.style.marginLeft = "10px";
+            // add event
+            deleteButton.addEventListener("click", () => {
                 shoppingList.removeChild(itemValue);
-                itemObject = itemObject.filter(item => item.name !== cleanInput);
-            };
-        })
-        // create + button
-        const addButton = document.createElement("button");
-        addButton.textContent = "+";
-        addButton.classList.add("addButton");
-        addButton.style.marginLeft = "10px";
-        // create count
-        const count = document.createElement("p");
-        count.textContent = newItem.quantity;
-        count.classList.add("count");
-        count.style.marginLeft = "10px";
-        // add in html
-        itemValue.appendChild(removeButton);
-        itemValue.appendChild(count);
-        itemValue.appendChild(addButton);
-        itemValue.appendChild(deleteButton);
-        shoppingList.appendChild(itemValue);
+                itemObject = itemObject.filter(
+                    (item) => item.name !== cleanInput
+                );
+            });
+            // create - button
+            const removeButton = document.createElement("button");
+            removeButton.textContent = "-";
+            removeButton.id = "removeButton";
+            removeButton.style.marginLeft = "10px";
+            // add event
+            removeButton.addEventListener("click", () => {
+                if (newItem.quantity > 1) {
+                    newItem.quantity--;
+                    count.textContent = newItem.quantity;
+                } else {
+                    shoppingList.removeChild(itemValue);
+                    itemObject = itemObject.filter(
+                        (item) => item.name !== cleanInput
+                    );
+                }
+            });
+            // create + button
+            const addButton = document.createElement("button");
+            addButton.textContent = "+";
+            addButton.id = "addButton";
+            addButton.style.marginLeft = "10px";
+            // add event
+            addButton.addEventListener("click", () => {
+                newItem.quantity++;
+                count.textContent = newItem.quantity;
+            });
+            // create count
+            const count = document.createElement("p");
+            count.textContent = newItem.quantity;
+            count.id = `count_${cleanInput}`;
+            count.style.marginLeft = "10px";
+            // add in html
+            itemValue.appendChild(removeButton);
+            itemValue.appendChild(count);
+            itemValue.appendChild(addButton);
+            itemValue.appendChild(deleteButton);
+            shoppingList.appendChild(itemValue);
         }
         // reset the input field
-        input.value  = ""; 
+        input.value = "";
     }
-
 };
 
 saveButton.addEventListener("click", (event) => {
-    event.preventDefault(); 
-    addItemToList()
+    event.preventDefault();
+    addItemToList();
 });
 
 input.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
         event.preventDefault();
-        addItemToList();    
+        addItemToList();
     }
+});
+
+clearAll.addEventListener("click", () => {
+    shoppingList.innerHTML = "";
+    itemObject = [];
 });
