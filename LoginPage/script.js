0d6d7ae425f84019dff464d9cf7fd8ccc6ddb5a6
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2024-09-12 15:38:51 +0200

    Add security

--- LoginPage/script.js
+++ LoginPage/script.js
@@ -1,16 +1,33 @@
 const username = document.getElementById("username");
 const password = document.getElementById("password");
 const submit = document.getElementById("submit");
+// Create a Data Base
+let BDD = [];
+// Hash function
+function stringToHash(string) {
 
+    let hash = 0;
+
+    if (string.length == 0) return hash;
+
+    for (i = 0; i < string.length; i++) {
+        char = string.charCodeAt(i);
+        hash = ((hash << 5) - hash) + char;
+        hash = hash & hash;
+    }
+
+    return hash;
+}
+// Submit the result
 submit.addEventListener("click", () => {
+    // Clean string
     user = username.value.trim();
     pass = password.value.trim();
-
-    if(user.lenght > 8){
-        console.log("AMAZING")
+    // Min 8 character for user & pass
+    if(user.length > 8 && pass.length > 8){
+        const newItem = {name: user, secretWord: stringToHash(pass)}
+        BDD.push(newItem);
+        username.value = "";
+        password.value = "";
     }
 })
-
-const checkData = () => {
-
-}
